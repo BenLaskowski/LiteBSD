8f39c658f617caed048f4a429a888dbcb809fd70
Ben Laskowski <ben.laskowski@gmail.com>
2018-07-14 20:55:51 -0400

    Add SPI2 PPS logic to machdep.c.  Soldered an SD breakout onto my MZDA starter kit.  The kernel boots!
        Copyright (c) 1982, 1986, 1989, 1991, 1993
                The Regents of the University of California.  All rights reserved.
    
        4.4BSD-Lite build 28 compiled 2018-07-14
            ben@server-pic32builder.localdomain:LiteBSD/sys/compile/MZDA_STARTER.pic32
        cpu: PIC32MZ2064DAG169 rev A1, 200 MHz
        oscillator: system PLL div 1:6 mult x50
        cache: 32/32 kbytes
        real mem = 640 kbytes
        avail mem = 472 kbytes
        using 18 buffers containing 73728 bytes of memory
        spi2 at pins sdi=RD7/sdo=RG8/sck=RG6
        en0 at interrupt 153, MAC address 54:10:ec:89:19:e9
        en0: <SMSC LAN8740A> at address 0
        uart2 at pins rx=RB0/tx=RG9, interrupts 145/146/147, console
        sd0 at port spi2, pin cs=RA9
        gpio0 at portA, pins ii---io-iiiiiiii
        gpio1 at portB, pins iiiiiiiiiiiiiiii
        gpio2 at portC, pins iooi-------iiii-
        gpio3 at portD, pins iiiiiii-ioiiiiii
        gpio4 at portE, pins ------iiiiiiiiii
        gpio5 at portF, pins --ii---i--iiiiii
        gpio6 at portG, pins iiii--iiii----ii
        gpio7 at portH, pins iiiiiiiiiiiiiiio
        gpio8 at portJ, pins iiiiiiooiiiiiiii
        gpio9 at portK, pins --------iiiiiiii
        sd0: function groups 1/1/1/1/4001/1
        sd0: type SDHC, size 15558144 kbytes, speed 12 Mbit/sec
        sd0a: partition type b7, sector 2, size 204800 kbytes
        sd0b: partition type b8, sector 409602, size 32768 kbytes
        sd0c: partition type b7, sector 475138, size 102400 kbytes
        WARNING: preposterous clock chip time -- CHECK AND RESET THE DATE!
    
        starting file system checks.
        /dev/rsd0a: file system is clean; not checking
        starting network
        add net 0: gateway 10.0.2.2
        clearing /tmp
        standard daemons: update cron inetd.
        Sat Jul 14 17:52:45 PDT 2018
    
        4.4BSD-Lite (bsd.net) (console)
    
        login:

--- sys/mips/pic32/machdep.c
+++ sys/mips/pic32/machdep.c
@@ -287,13 +287,17 @@ mach_init()
 #if defined(MZDA_STARTER)
 	/* Microchip MZ DA starter board:  use UART2 for console
 	 * Map signals rx=RB0 (RPB0/AN0), tx=RG9 (RPG9/AN23. */
-	SYSKEY = 0xAA996655;	/* unlock sequence so we can remap pins */
+	SYSKEY = 0xAA996655;    /* unlock sequence so we can remap pins */
 	SYSKEY = 0x556699AA;
 	CFGCON &= ~(1<<13);
-	ANSELBCLR = 1 << 0;		/* analog disable on RB0 and RG9 */
+	ANSELBCLR = 1 << 0;     /* analog disable on RB0 and RG9 */
 	ANSELGCLR = 1 << 9;
-	U2RXR = 5;				/* U2RX on RPB0 */
-	RPG9R = 2;				/* U2TX on RPG9 */
+	U2RXR = 5;              /* U2RX on RPB0 */
+	RPG9R = 2;              /* U2TX on RPG9 */
+    
+    /* set up SPI port here too - is this done elsewhere? */
+    SDI2R = 14;             /* SDI2 on RPD7 */
+    RPG8R = 6;              /* SDO2 on RPG8 */
 	CFGCON |= 1 << 13;		/* lock the config again */
 	SYSKEY = 0x33333333;
 #endif
